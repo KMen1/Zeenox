ff621ce3d8f92bd5136d153e18784ceb35e33551
KMen1 <oli70963@gmail.com>
2023-09-03 10:56:36 +0200

    refactor: CS-R1062

--- Zeenox/Modules/Music/Preconditions/RateLimitAttribute.cs
+++ Zeenox/Modules/Music/Preconditions/RateLimitAttribute.cs
@@ -11,111 +11,110 @@ using Discord.Interactions;
 using Discord.WebSocket;
 // ReSharper disable PossibleMultipleEnumeration
 
-namespace Zeenox.Modules.Music.Preconditions
+namespace Zeenox.Modules.Music.Preconditions;
+
+public class RateLimit : PreconditionAttribute
 {
-    public class RateLimit : PreconditionAttribute
+    public static ConcurrentDictionary<ulong, List<RateLimitItem>> Items = new ();
+    private static DateTime _removeExpiredCommandsTime = DateTime.MinValue;
+    private readonly RateLimitType? _context;
+    private readonly RateLimitBaseType _baseType;
+    private readonly int _requests;
+    private readonly int _seconds;
+    public RateLimit(int seconds = 4, int requests = 1, RateLimitType context = RateLimitType.User, RateLimitBaseType baseType = RateLimitBaseType.BaseOnCommandInfo)
     {
-        public static ConcurrentDictionary<ulong, List<RateLimitItem>> Items = new ();
-        private static DateTime _removeExpiredCommandsTime = DateTime.MinValue;
-        private readonly RateLimitType? _context;
-        private readonly RateLimitBaseType _baseType;
-        private readonly int _requests;
-        private readonly int _seconds;
-        public RateLimit(int seconds = 4, int requests = 1, RateLimitType context = RateLimitType.User, RateLimitBaseType baseType = RateLimitBaseType.BaseOnCommandInfo)
+        _context = context;
+        _requests = requests;
+        _seconds = seconds;
+        _baseType = baseType;
+    }
+    public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+    {
+        // clear old expired commands every 30m
+        if (DateTime.UtcNow > _removeExpiredCommandsTime)
         {
-            _context = context;
-            _requests = requests;
-            _seconds = seconds;
-            _baseType = baseType;
+            _ = Task.Run(async () =>
+            {
+                await ClearExpiredCommands().ConfigureAwait(false);
+                _removeExpiredCommandsTime = DateTime.UtcNow.AddMinutes(30);
+            });
         }
-        public override Task<PreconditionResult> CheckRequirementsAsync(IInteractionContext context, ICommandInfo commandInfo, IServiceProvider services)
+
+        ulong id = _context.Value switch
         {
-            // clear old expired commands every 30m
-            if (DateTime.UtcNow > _removeExpiredCommandsTime)
-            {
-                _ = Task.Run(async () =>
-                {
-                    await ClearExpiredCommands().ConfigureAwait(false);
-                    _removeExpiredCommandsTime = DateTime.UtcNow.AddMinutes(30);
-                });
-            }
+            RateLimitType.User => context.User.Id,
+            RateLimitType.Channel => context.Channel.Id,
+            RateLimitType.Guild => context.Guild.Id,
+            RateLimitType.Global => 0,
+            _ => 0
+        };
 
-            ulong id = _context.Value switch
-            {
-                RateLimitType.User => context.User.Id,
-                RateLimitType.Channel => context.Channel.Id,
-                RateLimitType.Guild => context.Guild.Id,
-                RateLimitType.Global => 0,
-                _ => 0
-            };
+        var contextId = _baseType switch
+        {
+            RateLimitBaseType.BaseOnCommandInfo => commandInfo.Module.Name + "//" + commandInfo.Name + "//" + commandInfo.MethodName,
+            RateLimitBaseType.BaseOnCommandInfoHashCode => commandInfo.GetHashCode().ToString(),
+            RateLimitBaseType.BaseOnSlashCommandName => (context.Interaction as SocketSlashCommand).CommandName,
+            RateLimitBaseType.BaseOnMessageComponentCustomId => (context.Interaction as SocketMessageComponent).Data.CustomId,
+            RateLimitBaseType.BaseOnAutocompleteCommandName => (context.Interaction as SocketAutocompleteInteraction).Data.CommandName,
+            RateLimitBaseType.BaseOnApplicationCommandName => (context.Interaction as SocketApplicationCommand).Name,
+            _ => "unknown"
+        };
 
-            var contextId = _baseType switch
-            {
-                RateLimitBaseType.BaseOnCommandInfo => commandInfo.Module.Name + "//" + commandInfo.Name + "//" + commandInfo.MethodName,
-                RateLimitBaseType.BaseOnCommandInfoHashCode => commandInfo.GetHashCode().ToString(),
-                RateLimitBaseType.BaseOnSlashCommandName => (context.Interaction as SocketSlashCommand).CommandName,
-                RateLimitBaseType.BaseOnMessageComponentCustomId => (context.Interaction as SocketMessageComponent).Data.CustomId,
-                RateLimitBaseType.BaseOnAutocompleteCommandName => (context.Interaction as SocketAutocompleteInteraction).Data.CommandName,
-                RateLimitBaseType.BaseOnApplicationCommandName => (context.Interaction as SocketApplicationCommand).Name,
-                _ => "unknown"
-            };
+        var dateTime = DateTime.UtcNow;
 
-            var dateTime = DateTime.UtcNow;
+        var target = Items.GetOrAdd(id, new List<RateLimitItem>());
 
-            var target = Items.GetOrAdd(id, new List<RateLimitItem>());
-
-            var commands = target.Where(
-                a =>
+        var commands = target.Where(
+            a =>
                 a.Command == contextId
-            );
+        );
 
-            foreach (var c in commands.ToList())
-                if (dateTime >= c.ExpireAt)
-                    target.Remove(c);
+        foreach (var c in commands.ToList())
+            if (dateTime >= c.ExpireAt)
+                target.Remove(c);
 
-            if (commands.Count() < _requests)
-            {
-                target.Add(new RateLimitItem()
-                {
-                    Command = contextId,
-                    ExpireAt = DateTime.UtcNow + TimeSpan.FromSeconds(_seconds)
-                });
-                return Task.FromResult(PreconditionResult.FromSuccess());
-            }
-
-            return Task.FromResult(PreconditionResult.FromError($"This command is usable <t:{((DateTimeOffset)target.Last().ExpireAt).ToUnixTimeSeconds()}:R>."));
-        }
-        public static Task ClearExpiredCommands()
+        if (commands.Count() < _requests)
         {
-            foreach (var doc in Items)
+            target.Add(new RateLimitItem()
             {
-                var utcTime = DateTime.UtcNow;
-                foreach (var command in doc.Value.Where(a => utcTime > a.ExpireAt).ToList())
-                    doc.Value.Remove(command);
-            }
-            return Task.CompletedTask;
+                Command = contextId,
+                ExpireAt = DateTime.UtcNow + TimeSpan.FromSeconds(_seconds)
+            });
+            return Task.FromResult(PreconditionResult.FromSuccess());
         }
+
+        return Task.FromResult(PreconditionResult.FromError($"This command is usable <t:{((DateTimeOffset)target.Last().ExpireAt).ToUnixTimeSeconds()}:R>."));
+    }
+    public static Task ClearExpiredCommands()
+    {
+        foreach (var doc in Items)
+        {
+            var utcTime = DateTime.UtcNow;
+            foreach (var command in doc.Value.Where(a => utcTime > a.ExpireAt).ToList())
+                doc.Value.Remove(command);
+        }
+        return Task.CompletedTask;
+    }
         
-        public class RateLimitItem
-        {
-            public string Command { get; set; } = null!;
-            public DateTime ExpireAt { get; set; }
-        }
-        public enum RateLimitType
-        {
-            User,
-            Channel,
-            Guild,
-            Global
-        }
-        public enum RateLimitBaseType
-        {
-            BaseOnCommandInfo,
-            BaseOnCommandInfoHashCode,
-            BaseOnSlashCommandName,
-            BaseOnMessageComponentCustomId,
-            BaseOnAutocompleteCommandName,
-            BaseOnApplicationCommandName
-        }
+    public class RateLimitItem
+    {
+        public string Command { get; set; } = null!;
+        public DateTime ExpireAt { get; set; }
+    }
+    public enum RateLimitType
+    {
+        User,
+        Channel,
+        Guild,
+        Global
+    }
+    public enum RateLimitBaseType
+    {
+        BaseOnCommandInfo,
+        BaseOnCommandInfoHashCode,
+        BaseOnSlashCommandName,
+        BaseOnMessageComponentCustomId,
+        BaseOnAutocompleteCommandName,
+        BaseOnApplicationCommandName
     }
 }
