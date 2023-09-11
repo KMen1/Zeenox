7065d24c54a145837d5cbc548220740da94b2a16
KMen1 <oli70963@gmail.com>
2023-09-11 19:46:15 +0200

    code: Add GuildInfo

---
+++ Zeenox/Models/GuildInfo.cs
@@ -0,0 +1,22 @@
+﻿using Discord.WebSocket;
+
+namespace Zeenox.Models;
+
+public class GuildInfo
+{
+    private GuildInfo(string id, string name, string iconUrl)
+    {
+        Id = id;
+        Name = name;
+        IconUrl = iconUrl;
+    }
+
+    public string Id { get; init; }
+    public string Name { get; init; }
+    public string IconUrl { get; init; }
+
+    public static GuildInfo FromSocketGuild(SocketGuild guild)
+    {
+        return new GuildInfo(guild.Id.ToString(), guild.Name, guild.IconUrl);
+    }
+}
